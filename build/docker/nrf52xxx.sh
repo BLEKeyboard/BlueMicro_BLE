ad42a6019da8c116e7aa66122a70abf804375603
Simon Johansson <simon@simonjohansson.com>
2020-05-20 12:11:36 +0200

    Make scripts executable and remove the absolute path to arduino-cli

--- build/docker/nrf52xxx.sh
+++ build/docker/nrf52xxx.sh
@@ -69,8 +69,8 @@ arduino_compile() {
    #sleep 2
 
    #Compile
-   cmdCompile832="/arduino-cli compile -v --fqbn adafruit:nrf52:feather52832 --build-path $buildPath --build-cache-path $buildCachePath $sourcePath/firmware.ino  -o $buildPath/firmware"
-   cmdCompile840="/arduino-cli compile -v --fqbn adafruit:nrf52:feather52840 --build-path $buildPath --build-cache-path $buildCachePath $sourcePath/firmware.ino  -o $buildPath/firmware"
+   cmdCompile832="arduino-cli compile -v --fqbn adafruit:nrf52:feather52832 --build-path $buildPath --build-cache-path $buildCachePath $sourcePath/firmware.ino  -o $buildPath/firmware"
+   cmdCompile840="arduino-cli compile -v --fqbn adafruit:nrf52:feather52840 --build-path $buildPath --build-cache-path $buildCachePath $sourcePath/firmware.ino  -o $buildPath/firmware"
     if $verbose; then 
       $cmdCompile832
     else
